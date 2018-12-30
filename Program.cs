97d251263b2fca0daf28bf050c2db3a362fcf627
Simon Aarons <40786398+simontime@users.noreply.github.com>
2018-12-30 18:38:51 +1100

    Add lifelight demo

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using CSCore;
+using CSCore.SoundOut;
+using System.IO;
+using System.IO.Compression;
+
+namespace scap
+{
+    class Program
+    {
+        static void Play(Stream stream)
+        {
+            using (IWaveSource soundSource = new CSCore.Codecs.AAC.AacDecoder(stream))
+            using (ISoundOut soundOut = new DirectSoundOut())
+            {
+                soundOut.Initialize(soundSource);
+                soundOut.Play();
+                while (soundOut.PlaybackState == PlaybackState.Playing) ;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            using (var ms = new MemoryStream())
+            using (var strm = new MemoryStream(Properties.Resources.sound))
+            using (var st = new DeflateStream(strm, CompressionMode.Decompress))
+            {
+                st.CopyTo(ms);
+                Play(ms);
+            }
+        }
+    }
+}
